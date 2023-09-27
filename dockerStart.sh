7fd28596c59cb11c6d4d16979c0178b37f66611e
Andrew Pegg <andrewcomputsci2019@gmail.com>
2023-09-26 20:49:15 -0700

    added port binding

--- dockerStart.sh
+++ dockerStart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "running start script"
 docker build --build-arg JAR_FILE=build/libs/*.jar -t mywebsite/app .
-docker run -d --restart unless-stopped --name website-container mywebsite/app
+docker run -d --restart unless-stopped --name website-container -p 8080:8080 mywebsite/app
 echo "exiting start script"
